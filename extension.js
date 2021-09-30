14174c5453b8c8a2949fd37d72995ea59167bd59
Leonardo Silveira <sombriks@gmail.com>
2021-09-29 22:43:12 -0300

    trying things

--- extension.js
+++ extension.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 const Main = imports.ui.main;
 const Config = imports.misc.config;
@@ -11,10 +11,34 @@ const PanelMenu = imports.ui.panelMenu;
 const PopupMenu = imports.ui.popupMenu;
 const GObject = imports.gi.GObject;
 
-function enable(){
-  log("hello world -- enable!");
+function disable() {
+  log("TEST hello world -- disable!");
+  if (shellHelloWorld) shellHelloWorld.destroy();
 }
 
-function disable(){
-  log("hello world -- disable!");
+let ShellHelloWorld = GObject.registerClass(
+  {
+    GTypeName: "ShellHelloWorld",
+  },
+  _ShellHelloWorld
+);
+
+class _ShellHelloWorld extends PanelMenu.Button {
+  _init() {
+    super._init(0.0, "Hello World");
+    const hbox = new St.BoxLayout({  });
+    const gicon = Gio.icon_new_for_string(Me.path + "/picture_09.jpg");
+    const icon = new St.Icon({ gicon: gicon, icon_size: "24" });
+
+    hbox.add_child(icon);
+    this.add_child(hbox);
+  }
+}
+
+let shellHelloWorld;
+
+function enable() {
+  log("TEST hello world -- enable!");
+  shellHelloWorld = new ShellHelloWorld();
+  Main.panel.addToStatusArea("shel-hello-world", shellHelloWorld);
 }
